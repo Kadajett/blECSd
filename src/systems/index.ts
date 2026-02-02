c69cd2e3d0d1ac36855d09363c7c9c2c6fcde554
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2026-02-02 09:59:46 -0500

    bd sync: 2026-02-02 09:59:46

--- src/systems/index.ts
+++ src/systems/index.ts
@@ -42,26 +42,6 @@ export {
 	registerCollisionSystem,
 	resetCollisionState,
 } from './collisionSystem';
-// Drag system
-export type {
-	DragConstraints,
-	DragEndEvent,
-	DragEventMap,
-	DragMoveEvent,
-	DragStartEvent,
-	DragState,
-	DragVerifyCallback,
-	DropEvent,
-} from './dragSystem';
-export {
-	clearDragConstraints,
-	createDragSystem,
-	getDragConstraints,
-	getDragVerifyCallback,
-	resetDragStores,
-	setDragConstraints,
-	setDragVerifyCallback,
-} from './dragSystem';
 // Input system
 export type {
 	HitTestResult,
@@ -92,19 +72,6 @@ export {
 	releaseMouse,
 	resetInputState,
 } from './inputSystem';
-// Layout system
-export type { ComputedLayoutData } from './layoutSystem';
-export {
-	ComputedLayout,
-	computeLayoutNow,
-	createLayoutSystem,
-	getComputedBounds,
-	getComputedLayout,
-	hasComputedLayout,
-	invalidateAllLayouts,
-	invalidateLayout,
-	layoutSystem,
-} from './layoutSystem';
 // Movement system
 export {
 	createMovementSystem,
@@ -114,22 +81,6 @@ export {
 	registerMovementSystem,
 	updateMovements,
 } from './movementSystem';
-// Render system
-export type { RenderContext } from './renderSystem';
-export {
-	clearRenderBuffer,
-	createRenderSystem,
-	getRenderBuffer,
-	markAllDirty,
-	renderBackground,
-	renderBorder,
-	renderContent,
-	renderRect,
-	renderScrollbar,
-	renderSystem,
-	renderText,
-	setRenderBuffer,
-} from './renderSystem';
 // State machine system
 export {
 	createStateMachineSystem,
@@ -141,28 +92,3 @@ export {
 	stateMachineSystem,
 	updateStateAges,
 } from './stateMachineSystem';
-// Output system
-export type { OutputState } from './outputSystem';
-export {
-	cleanup,
-	clearOutputBuffer,
-	clearOutputStream,
-	clearScreen,
-	createOutputState,
-	createOutputSystem,
-	cursorHome,
-	enterAlternateScreen,
-	generateOutput,
-	getOutputBuffer,
-	getOutputStream,
-	getOutputState,
-	hideCursor,
-	leaveAlternateScreen,
-	outputSystem,
-	resetAttributes,
-	resetOutputState,
-	setOutputBuffer,
-	setOutputStream,
-	showCursor,
-	writeRaw,
-} from './outputSystem';
