3e909d5a95c3d79747f984cce20f14177319aedb
Jeremy Stover <jeremy.ryan.stover@gmail.com>
2026-02-02 10:23:58 -0500

    bd sync: 2026-02-02 10:23:58

--- src/terminal/index.ts
+++ src/terminal/index.ts
@@ -82,30 +82,6 @@ export {
 	unpackRgb,
 	visibleLength,
 } from './ansi/parser';
-// Capability negotiation (internal)
-export type {
-	CapabilityNegotiator,
-	GraphicsProtocolValue,
-	KittyKeyboardLevelValue,
-	NegotiationTimingValue,
-	NegotiatorConfig,
-	TerminalCapabilities,
-} from './capabilities';
-export {
-	capabilityQuery,
-	createCapabilityNegotiator,
-	DEFAULT_QUERY_TIMEOUT,
-	GraphicsProtocol,
-	getDefaultNegotiator,
-	getTerminalCapabilities,
-	hasCapability,
-	KittyKeyboardLevel,
-	MAX_QUERY_TIMEOUT,
-	MIN_QUERY_TIMEOUT,
-	NegotiationTiming,
-	NegotiatorConfigSchema,
-	resetDefaultNegotiator,
-} from './capabilities';
 // Cleanup and signal handling (internal)
 export type { CleanupHandler, ExitHandler, ExitInfo, ExitReason } from './cleanup';
 export {
@@ -230,32 +206,6 @@ export {
 	query,
 	ResponseType,
 } from './responseParser';
-// Screen buffer and cell management
-export type { AttrFlags, Cell, CellChange, ScreenBufferData } from './screen';
-export {
-	Attr,
-	cellIndex,
-	cellsEqual,
-	clearBuffer,
-	cloneCell,
-	copyRegion,
-	createCell,
-	createScreenBuffer,
-	DEFAULT_BG as CELL_DEFAULT_BG,
-	DEFAULT_CHAR,
-	DEFAULT_FG as CELL_DEFAULT_FG,
-	diffBuffers,
-	fillRect,
-	getCell,
-	hasAttr,
-	isInBounds,
-	resizeBuffer,
-	setCell,
-	setChar,
-	withAttr,
-	withoutAttr,
-	writeString,
-} from './screen';
 // Screen buffer management (internal)
 export type { CleanupCallback } from './screenBuffer';
 export { ScreenBuffer } from './screenBuffer';
@@ -277,30 +227,3 @@ export { SuspendManager, suspend, suspendSequences } from './suspend';
 // Synchronized output (internal)
 export type { SyncOutputOptions } from './syncOutput';
 export { isSyncOutputSupported, SynchronizedOutput } from './syncOutput';
-// Terminfo (internal)
-export type {
-	BooleanCapability,
-	NumberCapability,
-	StringCapability,
-	TerminfoData,
-	Tput,
-	TputConfig,
-} from './terminfo';
-export { createTput, getDefaultTput, getDefaultXtermData, resetDefaultTput } from './terminfo';
-// Resize handling
-export type {
-	ResizeEventData,
-	ResizeEventMap,
-	ResizeHandler,
-	ResizeHandlerState,
-} from './resize';
-export {
-	createResizeHandler,
-	disableResizeHandling,
-	enableResizeHandling,
-	getResizeEventBus,
-	getResizeHandler,
-	resetResizeEventBus,
-	setupSigwinchHandler,
-	triggerResize,
-} from './resize';
